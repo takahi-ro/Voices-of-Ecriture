84f0dcba3df6fbdb654f861082ebfa70da7aba44
Takahiro Fujii <57240543+takahi-ro@users.noreply.github.com>
2025-01-01 20:20:30 +0900

    文言の微修正

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>エクリチュールの声（<a target="_blank" href = "https://github.com/takahi-ro/Voices-of-Ecriture/blob/main/README.md">Github</a>）</h1>
-      <p>「<a target="_blank" href = "https://neort.io/art/ce3k81sn70rlpj69c980">エクリチュールの声</a>」という過去にp5.jsで作った作品を、誰でも体験可能なWebアプリにしました。また、LLM(大規模言語モデル)のアンチテーゼというメッセージを、本アプリには込めています。</p>
+      <p>「<a target="_blank" href = "https://neort.io/art/ce3k81sn70rlpj69c980">エクリチュールの声</a>」という過去にp5.jsで作った作品を、誰でも体験可能なWebアプリにしました。また、LLM（大規模言語モデル）のアンチテーゼというメッセージを、本アプリには込めています。</p>
       <SketchWrapper />
     </div>
   );
